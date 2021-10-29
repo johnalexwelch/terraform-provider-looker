598314b2991175eb0862a6e54c763dae651d5fd0
Alex <alex.welch@tastytrade.com>
2021-10-29 11:03:08 -0500

    updated to v4

--- pkg/looker/resource_project.go
+++ pkg/looker/resource_project.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/looker-open-source/sdk-codegen/go/rtl"
-	apiclient "github.com/looker-open-source/sdk-codegen/go/sdk/v3"
+	apiclient "github.com/looker-open-source/sdk-codegen/go/sdk/v4"
 )
 
 func resourceProject() *schema.Resource {
